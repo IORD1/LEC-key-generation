f023913bb12636065d6066c1c8ff3f2e6e73f8d5
IORD1 <pratham111ingole@gmail.com>
2022-09-28 00:48:34 +0530

    able to encrypt and decrypt

---
+++ encryption/encryptit.py
@@ -0,0 +1,55 @@
+import cryptography
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+
+with open("public_key.pem", "rb") as key_file:
+    public_key = serialization.load_pem_public_key(
+        key_file.read(),
+        backend=default_backend()
+    )
+
+message = b'encrypt me!'
+# Use one of the methods above to get your public key
+
+encrypted = public_key.encrypt(
+    message,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+
+print((encrypted));
+
+#WRITING ENCRYPTED DATA TO A FILE
+with open('encrypted.pem', 'wb') as f:
+    f.write(encrypted)
+
+
+
+
+#READING PRIVATE KEY FROM FILE
+
+with open("private_key.pem", "rb") as key_file:
+    private_key = serialization.load_pem_private_key(
+        key_file.read(),
+        password=None,
+        backend=default_backend()
+    )
+# DECRYPTING MESSAGE FROM ENCRYPTED FILE
+
+original_message = private_key.decrypt(
+    encrypted,
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+print('----------------------------------------------------------------')
+print(original_message)
+
+
